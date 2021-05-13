acbcc7975911958edc7f6ba83d8e5b2020ed66e1
sunyton <sunytoncxw@gmail.com>
2021-05-13 19:37:14 +0800

    debug error

--- main.ts
+++ main.ts
@@ -106,16 +106,16 @@ namespace Arducam {
         }
 
         wrSensorReg8_8(0xff, 0x01)
-        wrSensorReg8_8(0x12, 0x80)
-        basic.pause(100);
-        // format = IMAGE_FORMAT.JPEG
-        // if (format == IMAGE_FORMAT.JPEG) {
-            wrSensorRegs8_8(OV2640_JPEG_INIT)
-            wrSensorRegs8_8(OV2640_YUV422);
-            wrSensorRegs8_8(OV2640_JPEG);
-            wrSensorReg8_8(0xff, 0x01);
-            wrSensorReg8_8(0x15, 0x00);
-            wrSensorRegs8_8(OV2640_320x240_JPEG);
+        // wrSensorReg8_8(0x12, 0x80)
+        // basic.pause(100);
+        // // format = IMAGE_FORMAT.JPEG
+        // // if (format == IMAGE_FORMAT.JPEG) {
+        //     wrSensorRegs8_8(OV2640_JPEG_INIT)
+        //     wrSensorRegs8_8(OV2640_YUV422);
+        //     wrSensorRegs8_8(OV2640_JPEG);
+        //     wrSensorReg8_8(0xff, 0x01);
+        //     wrSensorReg8_8(0x15, 0x00);
+        //     wrSensorRegs8_8(OV2640_320x240_JPEG);
             basic.showIcon(IconNames.Sad)
         // } else {
             // wrSensorRegs8_8(OV2640_QVGA);
